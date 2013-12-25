3c9a37d128ad57f347706e42a54aa2f5df9968ab
stefan.hermelin <stefan.hermelin@klarna.com>
2013-12-25 20:33:36 +0100

    Simple list of transactions.

--- src/main/java/com/freece/model/Transaction.java
+++ src/main/java/com/freece/model/Transaction.java
@@ -1,19 +1,29 @@
 package com.freece.model;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Transaction {
-    final private String note;
+    final private Date date;
+    final private String description;
     final private int amount;
 
-    public Transaction(String note, int amount) {
-        this.note = note;
+    public Transaction(Date date, String description, int amount) {
+        this.date = date;
+        this.description = description;
         this.amount = amount;
     }
 
-    public String getNote() {
-        return note;
+    public String getDescription() {
+        return description;
     }
 
     public int getAmount() {
         return amount;
+    }
+
+    public String getFormattedDate() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+        return format.format(date);
     }
 }
--- src/main/java/com/freece/pages/ListTransactionsPage.java
+++ src/main/java/com/freece/pages/ListTransactionsPage.java
@@ -1,24 +1,42 @@
 package com.freece.pages;
 
+import com.freece.model.Transaction;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-/**
- * Created with IntelliJ IDEA.
- * User: stefan.hermelin
- * Date: 12/23/13
- * Time: 10:06 PM
- * To change this template use File | Settings | File Templates.
- */
-public class ListTransactionsPage extends AbstractBasePage {
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+public class ListTransactionsPage extends WebPage {
 
     private static final long serialVersionUID = 1L;
+    private IModel<Transaction> model;
 
     public ListTransactionsPage(final PageParameters parameters) {
         super(parameters);
 
-//        add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+        List list = Arrays.asList(
+                new Transaction[] {
+                        new Transaction(new Date(), "SevenElevent", 54),
+                        new Transaction(new Date(), "Teknikmagasinet", 199),
+                        new Transaction(new Date(), "Statoil", 756)
+                }
+        );
+        ListView listview = new ListView("listView", list) {
+            protected void populateItem(ListItem item) {
+                Transaction transaction = (Transaction) item.getModelObject();
+                item.add(new Label("tDate", transaction.getFormattedDate()));
+                item.add(new Label("tDesc", transaction.getDescription()));
+                item.add(new Label("tAmount", transaction.getAmount()));
+            }
+        };
+        add(listview);
 
-        // TODO Add your page's components here
 
     }
 }
